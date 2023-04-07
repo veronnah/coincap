6f3e0a4f4802b8d868ca3ccb878acaac3963182b
veronnah <yoo.veroon@gmail.com>
2023-04-07 23:07:33 +0200

    added http params

--- src/app/services/coins.service.ts
+++ src/app/services/coins.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { map, Observable } from "rxjs";
 import { environment } from "../../environments/environment";
 import { CoinModel } from "../models/coin.model";
 import { CoinDetailsModel } from "../models/coinDetails.model";
 import { MarketDataModel } from "../models/marketData.model";
+import { SearchCoinsModel } from "../models/searchCoins.model";
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,16 @@ export class CoinsService {
   }
 
   public getCoinsList(pageNumber: number): Observable<CoinModel[]> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('vs_currency', 'usd');
+    httpParams = httpParams.append('order', 'market_cap_desc');
+    httpParams = httpParams.append('per_page', '100');
+    httpParams = httpParams.append('page', pageNumber);
+    httpParams = httpParams.append('sparkline', 'true');
+    httpParams = httpParams.append('price_change_percentage', '1h,24h,7d,14d');
+
     return this.http.get<CoinModel[]>(
-      `${environment.apiUrl}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${pageNumber}&sparkline=true&price_change_percentage=1h%2C%2024h%2C%207d%2C14d`)
+      `${environment.apiUrl}/coins/markets`, {params: httpParams})
       .pipe(map((result: CoinModel[]) => {
           return result.map((coin: CoinModel) => {
             return {
@@ -41,11 +50,16 @@ export class CoinsService {
     return this.http.get<CoinDetailsModel>(`${environment.apiUrl}/coins/${id}`);
   }
 
-  public searchCoins(searchValue: string): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}/search?query=${searchValue}`);
+  public searchCoins(searchValue: string): Observable<SearchCoinsModel> {
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('query', searchValue);
+    return this.http.get<SearchCoinsModel>(`${environment.apiUrl}/search`, {params: httpParams});
   }
 
   public getMarketData(coinId: string, days: string | undefined): Observable<MarketDataModel> {
-    return this.http.get<MarketDataModel>(`${environment.apiUrl}/coins/${coinId}/market_chart?vs_currency=usd&days=${days}`);
+    let httpParams = new HttpParams();
+    httpParams = httpParams.append('vs_currency', 'usd');
+    httpParams = httpParams.append('days', days!);
+    return this.http.get<MarketDataModel>(`${environment.apiUrl}/coins/${coinId}/market_chart`, {params: httpParams});
   }
 }
